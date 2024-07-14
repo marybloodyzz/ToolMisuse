34142526aabe7b41ac25c01ffba954f45c2ad0bf
marybloodyzz <112670928+marybloodyzz@users.noreply.github.com>
2024-07-15 00:01:01 +0800

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,4 @@
 conda create -n tool_misuse python=3.10
 conda activate tool_misuse
 conda install -y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-pip install opencv-python tqdm bs4 sentencepiece openai-clip timm transformers
+pip install opencv-python tqdm bs4 sentencepiece openai-clip timm transformers==4.40.0 accelerate tiktoken
